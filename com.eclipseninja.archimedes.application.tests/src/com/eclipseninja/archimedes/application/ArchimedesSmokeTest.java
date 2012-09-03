d7a81f589110bec3293f2aafd7d3693b1cc97453
Jon Kurisu <jkurisu@gmail.com>
2012-09-03 12:20:56 -0700

    adding logging and target update site build to project

---
+++ com.eclipseninja.archimedes.application.tests/src/com/eclipseninja/archimedes/application/ArchimedesSmokeTest.java
@@ -0,0 +1,34 @@
+package com.eclipseninja.archimedes.application;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ArchimedesSmokeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertNotNull("Not yet implemented");
+	}
+
+}
--- com.eclipseninja.archimedes.application/src/com/eclipseninja/archimedes/application/Activator.java
+++ com.eclipseninja.archimedes.application/src/com/eclipseninja/archimedes/application/Activator.java
@@ -1,9 +1,14 @@
 package com.eclipseninja.archimedes.application;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
+	
+	private static final Logger logger =  LoggerFactory.getLogger(Activator.class); 
 
 	private static BundleContext context;
 
@@ -16,6 +21,8 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
+		Bundle bundle = bundleContext.getBundle();
+		logger.debug("Starting: " + bundle.getSymbolicName());
 		Activator.context = bundleContext;
 	}
 
@@ -24,6 +31,8 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
+		Bundle bundle = bundleContext.getBundle();
+		logger.debug("Stopping: " + bundle.getSymbolicName());
 		Activator.context = null;
 	}
 
